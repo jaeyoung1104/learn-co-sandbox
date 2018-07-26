ef05dee2185cc2cba0a945937572c96792bae74e
Jae Young Jeong <jaeyoung1104@gmail.com>
2018-07-26 23:39:23 +0000

    automatically backed up by learn

---
+++ collection.py
@@ -0,0 +1,13 @@
+nested_list = [1, [2, [4, [5, [6]], 3]]]
+number_six = None
+
+# 2. Sort the cats list in alphabetical order
+
+cats = ['Maine Coon', 'Tabby', 'Siamese', 'Garfield', 'Sylvester']
+
+# 3. Look up a new string or list method and explain how it work
+
+dogs = ['Scooby', 'Scrappy', 'Clifford', 'Pickles', 'Floyd']
+animal = None
+cats_jrs = []
+for i in cats: cats_jrs = cats[i] + " Jrs"
